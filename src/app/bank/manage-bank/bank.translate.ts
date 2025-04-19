5d8efaa89d48be7cd0bc4df5a520fbaeb04fe2ad
tonbao0803 <tonbao0803@gmail.com>
2025-04-19 21:44:29 +0700

    mod. change data in transaction list

--- src/app/bank/manage-bank/bank.translate.ts
+++ src/app/bank/manage-bank/bank.translate.ts
@@ -1,1 +1,57 @@
-export const TEXT_TRANSLATE = {};
+export const TEXT_TRANSLATE = {
+  TITLE: {
+    ACCOUNT_LIST: "Danh sách tài khoản",
+    ACCOUNT_DETAIL: "Chi tiết tài khoản",
+    TRANSACTION_HISTORY: "Lịch sử giao dịch",
+    TRANSACTION: "giao dịch",
+    ACCOUNT: "Tài khoản",
+    DESCRIPTION: "Mô tả",
+    RECEIVER: "Người nhận",
+    TRANSACTION_DIRECTION: "Số tiền",
+    TRANSACTION_TYPE: "Loại giao dịch",
+    TRANSACTION_STATUS: "Trạng thái",
+    SUCCESS_STATUS: "Thành công",
+    FAILURE_STATUS: "Thất bại",
+    TRANSACTION_DATE: "Ngày giao dịch",
+    STT: "STT",
+    ACCOUNT_NUMBER: "Số tài khoản",
+    ACCOUNT_HOLDER: "Chủ tài khoản",
+    BALANCE: "Số dư hiện tại",
+    CREATED_AT: "Ngày tạo",
+    ACTIONS: "Chức năng",
+    STATISTIC_SYSTEM: "Thống kê hệ thống",
+    USER: "Người dùng",
+    INSTRUCTION: "Hướng dẫn sử dụng",
+    INSTRUCTION_1: "Tạo tài khoản mới bằng cách điền thông tin bên phải",
+    INSTRUCTION_2:
+      "Quản lý tài khoản với các thao tác nạp/rút tiền/chuyển tiền",
+    INSTRUCTION_3: "Xem chi tiết lịch sử giao dịch của mỗi tài khoản",
+    BANK_ACCOUNT_INSTRUCTION: "Quản lý tài khoản ngân hàng",
+    BANK_ACCOUNT_SYSTEM_INSTRUCTION:
+      "Hệ thống quản lý tài khoản và giao dịch ngân hàng",
+    NO_CREATED_ACCOUNT: "Chưa có tài khoản nào được tạo.",
+    ACCOUNT_LIST_UPPERCASE: "Danh Sách Tài Khoản",
+    SEARCH_ACCOUNT: "Tìm kiếm tài khoản...",
+    INITIAL_BALANCE: "Số dư ban đầu",
+    INPUT_ACCOUNT_NUMBER: "Nhập số tài khoản",
+  },
+
+  MESSAGE: {
+    CONFRIM_DELETE: "Bạn có chắc muốn xóa tài khoản ngân hàng này không?",
+    DESCRIPTION_CONFIRM_DELETE: "Hành động này không thể hoàn tác.",
+  },
+
+  TOOLTIP: {
+    CANCEL: "Hủy",
+    DELETE: "Xóa",
+  },
+
+  BUTTON: {
+    DEPOSIT: "Nạp tiền",
+    WITHDRAW: "Rút tiền",
+    TRANSFER: "Chuyển tiền",
+    CREATE_NEW_ACCOUNT: "Tạo Tài Khoản Mới",
+    CREATE_ACCOUNT: "Tạo Tài Khoản",
+    CREATED: "Tạo",
+  },
+};
--- src/types/transaction.types.ts
+++ src/types/transaction.types.ts
@@ -1,12 +1,17 @@
 export interface Transaction {
+  id: string;
   sourceAccountId: string;
   destinationAccountId: string | null;
+  sourceAccountNumber: string;
+  sourceAccountHolder: string;
+  destinationAccountNumber: string;
+  destinationAccountHolder: string;
   amount: number;
   type: number;
   description: string;
   transactionDate: string;
   status: string;
-  id: string;
+  transactionDirection: number;
   createdAt: string;
   updatedAt: string | null;
   isDeleted: boolean;
